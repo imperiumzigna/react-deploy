a083c8389ee814257c1e5c2d00c0cb9f2d7db799
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2019-07-16 20:23:25 -0300

    initial deploy

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Escola de Javascript</b> meu deploy react! 😎
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://escoladejavascript.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visitar Artigo
         </a>
       </header>
     </div>
